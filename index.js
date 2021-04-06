7023a2204d19fe029034baeeb0155a4597700b09
fabogit <fabrizio.bosco.email@gmail.com>
2021-04-06 20:48:03 +0200

    testing mongo atlas connection

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-// server
-const express = require("express")
+// import
+const express = require('express')
+const mongoose = require('mongoose')
 
+// globals
+const MONGO = require('./variables.js')
+
+const mongo_user = MONGO.mongo.mongo_user
+const mongo_pwd = MONGO.mongo.mongo_pwd
+const mongo_dbs = MONGO.mongo.mongo_dbs
+const mongo_coll = MONGO.mongo.mongo_coll 
+
+// connect to mongo
+mongoose.connect(
+  "mongodb+srv://${mongo_user}:<${mongo_pwd}>@cluster0.x8yyx.mongodb.net/${mongo_dbs}?retryWrites=true&w=majority",
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+},
+() => console.log("connected to db")
+)
+
+// server
 const app = express();
 
 // import routes
 const authRoutes = require("./routes/auth")
+
 
 // route middlewares
 app.use("/auth", authRoutes)
 
 
 app.listen(3000, () => console.log("server is running..."))
+
+
+
+
+
+
+
+
+
+
+// // mongo connection
+// const uri = "mongodb+srv://${mongo_user}:<${mongo_pwd}>@cluster0.x8yyx.mongodb.net/${mongo_dbs}?retryWrites=true&w=majority"
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   () => console.log("connected to db")
+//   client.close();
+// })
---
+++ variables.js
@@ -0,0 +1,11 @@
+// project variables
+
+module.exports = {
+    // mongo credentials
+    mongo: {
+    mongo_user: "user_admin",
+    mongo_pwd: "4PMuXKJYgSsvsVP",
+    mongo_dbs: "mongo_database",
+    mongo_coll: "mongo_collection"
+}
+}
